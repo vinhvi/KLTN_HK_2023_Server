173bda72b2373efe706e6131fe5ba6234a1c9942
vinhvi <69356916+vinhvi@users.noreply.github.com>
2023-09-28 22:53:22 +0700

    fix bug ProductSpecificationController

--- src/main/java/com/example/demo/controller/ProductSpecificationController.java
+++ src/main/java/com/example/demo/controller/ProductSpecificationController.java
@@ -2,22 +2,25 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Product;
 import com.example.demo.entity.ProductSpecification;
+import com.example.demo.service.ProductService;
 import com.example.demo.service.ProductSpecificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/productSpecifications")
 public class ProductSpecificationController {
     private final ProductSpecificationService productSpecificationService;
-
-    @PostMapping("/saveOrUpdate")
-    public ResponseEntity<?> saveOrUpdate(@RequestBody ProductSpecification productSpecification) {
+    private final ProductService productService;
+    @PostMapping("/saveOrUpdate/{productId}")
+    public ResponseEntity<?> saveOrUpdate(@RequestBody ProductSpecification productSpecification, @PathVariable("productId") String productId) {
+        Product product =  productService.getById(productId);
+        if (product == null){
+            return ResponseEntity.badRequest().body("productId not found !!");
+        }
+        productSpecification.setProduct(product);
         ProductSpecification check = productSpecificationService.saveOrUpdate(productSpecification);
         if (check == null) {
             return ResponseEntity.badRequest().body("failed !!");
