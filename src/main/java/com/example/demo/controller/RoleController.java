19c4146ece9d64aeb9eccda927fe90677804158d
vinhvi <69356916+vinhvi@users.noreply.github.com>
2023-10-03 00:03:46 +0700

    add try catch for RoleController

--- src/main/java/com/example/demo/controller/RoleController.java
+++ src/main/java/com/example/demo/controller/RoleController.java
@@ -15,26 +15,44 @@ public class RoleController {
     private final RoleService roleService;
 
     @PostMapping("/create")
-    public ResponseEntity<Role> create(@RequestBody Role role) {
-        return ResponseEntity.ok().body(roleService.createRole(role));
+    public ResponseEntity<?> create(@RequestBody Role role) {
+        try {
+            return ResponseEntity.ok().body(roleService.createRole(role));
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body("There is an exception when execute !! --> " + exception);
+        }
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Role> getById(@PathVariable("id") int id) {
-        return ResponseEntity.ok().body(roleService.getById(id));
+    public ResponseEntity<?> getById(@PathVariable("id") int id) {
+        try {
+            return ResponseEntity.ok().body(roleService.getById(id));
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body("There is an exception when execute !! --> " + exception);
+        }
+
     }
 
     @GetMapping("/getByName/{name}")
     public ResponseEntity<?> getByName(@PathVariable("name") String name) {
-        Role role = roleService.getByName(name);
-        if (role == null) {
-            return ResponseEntity.badRequest().body(name + " not found!");
+        try {
+            Role role = roleService.getByName(name);
+            if (role == null) {
+                return ResponseEntity.badRequest().body(name + " not found!");
+            }
+            return ResponseEntity.ok().body(role);
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body("There is an exception when execute !! --> " + exception);
         }
-        return ResponseEntity.ok().body(role);
-    }
 
+    }
     @GetMapping("/getList")
-    public ResponseEntity<List<Role>> getAll() {
-        return ResponseEntity.ok().body(roleService.getAll());
+    public ResponseEntity<?> getAll() {
+        try {
+            return ResponseEntity.ok().body(roleService.getAll());
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body("There is an exception when execute !! --> " + exception);
+        }
+
     }
 }
