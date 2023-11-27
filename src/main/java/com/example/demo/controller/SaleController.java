729e585d23a27204164b687fed7ad1b1084eb412
vinhvi <69356916+vinhvi@users.noreply.github.com>
2023-11-28 00:49:15 +0700

    update api for Sale

---
+++ src/main/java/com/example/demo/controller/SaleController.java
@@ -0,0 +1,81 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Product;
+import com.example.demo.entity.Sale;
+import com.example.demo.service.ProductService;
+import com.example.demo.service.SaleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/sales")
+public class SaleController {
+    private final SaleService service;
+    private final ProductService productService;
+
+    @PostMapping("/saveOrUpdate")
+    public ResponseEntity<?> saveOrUpdate(@RequestBody Sale sale) {
+        try {
+            return ResponseEntity.ok().body(service.saveOrUpdate(sale));
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body("There is an exception when execute !! --> " + exception);
+        }
+    }
+
+    @GetMapping("/getById/{idKM}")
+    public ResponseEntity<?> getById(@PathVariable("idKM") String idKM) {
+        try {
+            Sale sale = service.getById(idKM);
+            if (sale == null) {
+                return ResponseEntity.badRequest().body(idKM + " not found!");
+            }
+            return ResponseEntity.ok().body(sale);
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body("There is an exception when execute !! --> " + exception);
+        }
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<?> getAll() {
+        try {
+            List<Sale> saleList = service.getAll();
+            if (saleList.isEmpty()) {
+                return ResponseEntity.badRequest().body(" not found!");
+            }
+            return ResponseEntity.ok().body(saleList);
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body("There is an exception when execute !! --> " + exception);
+        }
+    }
+
+    @GetMapping("/getSaleByProduct/{idProduct}")
+    public ResponseEntity<?> getByProduct(@PathVariable("idProduct") String idProduct) {
+        try {
+            Product product = productService.getById(idProduct);
+            List<Sale> sales = service.getByProduct(product);
+            if (sales.isEmpty()) {
+                return ResponseEntity.badRequest().body("not found sale for " + product.getId());
+            }
+            return ResponseEntity.ok().body(sales);
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body("There is an exception when execute !! --> " + exception);
+        }
+    }
+
+    @GetMapping("/addSaleFoProduct/{idKM}")
+    public ResponseEntity<?> addSaleFoProduct(@PathVariable("idKM") String idKM, List<Product> products) {
+        try {
+            Sale sale = service.getById(idKM);
+            List<Product> productList = sale.getProducts();
+            productList.addAll(products);
+            sale.setProducts(productList);
+            return ResponseEntity.ok().body(service.saveOrUpdate(sale));
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body("There is an exception when execute !! --> " + exception);
+        }
+    }
+}
